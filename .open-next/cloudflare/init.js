248b8f67524253b57679a80c5f7aad16682be6fe
root <root@localhost.localdomain>
2026-03-21 04:12:45 +0800

    fix: Pro email support shows as unavailable, remove API access from Unlimited

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1774037218837,
+    __BUILD_TIMESTAMP_MS__: 1774037534056,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
